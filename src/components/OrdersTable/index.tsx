94c356cc2e63c895e16c3e57e761d16457637e1d
Tomasz Ogonowski <tomasz.ogonowski@visionect.io>
2022-03-07 14:33:10 +0100

    style table

--- src/components/OrdersTable/index.tsx
+++ src/components/OrdersTable/index.tsx
@@ -1,12 +1,13 @@
 import { useSelector } from 'react-redux'
 import { RootState } from '../../state';
+import styles from './style.module.scss';
 
 export const OrdersTable = () => {
    const ordersList = useSelector((state: RootState) => state.workOrders.ordersList);
    return (
       <>
          {ordersList.length > 0 && (
-            <table>
+            <table className={styles.tableContainer}>
                <thead>
                   <tr>
                      <th>WO ID</th>
@@ -29,7 +30,7 @@ export const OrdersTable = () => {
                               {order.assigned_to.map(assigner => (
                                  <li key={assigner.person_name}>
                                     <div>{assigner.person_name}</div>
-                                    <div>{assigner.status}</div>
+                                    <span>{assigner.status}</span>
                                  </li>
                               ))}
                            </ul>
